61dd524cfe045d56e048dfd5eca640d988b203d7
nobuyuu <yu-ya_03_20_nobu@outlook.com>
2023-07-10 19:09:30 +0900

    shop edit,update

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -65,9 +65,9 @@ class OwnersController extends Controller
     {
         //$request->name;
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:owners',
+            'password' => 'required|string|confirmed|min:8',
         ]);
 
         try{
--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -25,7 +25,7 @@ class ShopController extends Controller
 
             $id = $request->route()->parameter('shop'); //shopのid取得
             if(!is_null($id)){ // null判定
-            $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
                 $shopId = (int)$shopsOwnerId; // キャスト 文字列→数値に型変換
                 $ownerId = Auth::id();
                 if($shopId !== $ownerId){ // 同じでなかったら
@@ -56,11 +56,31 @@ class ShopController extends Controller
 
     public function update(UpLoadImageRequest $request, $id)
     {
+
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'information' => 'required|string|max:1000',
+            'is_selling' => 'required',
+        ]);
+
         $imageFile = $request->image; //一時保存
-        if(!is_null($imageFile) && $imageFile->isValid() ){
+        if(!is_null($imageFile) && $imageFile->isValid()){
             $fileNameToStore = ImageService::upload($imageFile, 'shops');
         }
 
-        return redirect()->route('owner.shops.index');
+        $shop = Shop::findOrFail($id);
+        $shop->name = $request->name;
+        $shop->information = $request->information;
+        $shop->is_selling = $request->is_selling;
+        if(!is_null($imageFile) && $imageFile->isValid()){
+            $shop->filename = $fileNameToStore;
+        }
+
+        $shop->save();
+
+        return redirect()
+        ->route('owner.shops.index')
+        ->with(['message' => '店舗情報を更新しました。',
+        'status' => 'info']);
     }
 }

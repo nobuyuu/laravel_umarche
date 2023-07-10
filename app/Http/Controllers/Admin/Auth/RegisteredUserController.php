58b07003f86c6c034d0350e777576574b1a24f5f
nobuyuu <yu-ya_03_20_nobu@outlook.com>
2023-07-10 23:31:16 +0900

    image初期設定

--- app/Http/Controllers/Admin/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Admin/Auth/RegisteredUserController.php
@@ -34,9 +34,12 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:admins'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:admins',
+            'password' => 'required|string|confirmed|min:8',
+            // 'name' => ['required', 'string', 'max:255'],
+            // 'email' => ['required', 'string', 'email', 'max:255', 'unique:admins'],
+            // 'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = Admin::create([

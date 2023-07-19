ac93329f6c887b9df296ab2b7f0c723cece3f3b3
nobuyuu <yu-ya_03_20_nobu@outlook.com>
2023-07-19 19:54:41 +0900

    product edit

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -14,6 +14,8 @@
   if ($name === 'image5') {
     $modal = 'modal-5';
   }
+  $cImage = $currentImage ?? '' ;
+  $cId = $currentId ?? '' ;
 
 
 @endphp
@@ -34,7 +36,7 @@
                       <img class="image" data-id="{{ $name }}_{{ $image->id }}"
                       data-file="{{ $image->filename }}"
                       data-path="{{ asset('storage/products/') }}"
-                      data-micromodal-close {{--data-modal="{{ $modal }}"--}}
+                      data-modal="{{ $modal }}"
                       src="{{ asset('storage/products/' . $image->filename)}}" >
                         <div class="text-gray-700">
                             {{ $image->title }}
@@ -54,7 +56,7 @@
 <div class="flex justify-around items-center mb-4">
   <a class="py-2 px-4 bg-gray-200" data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイルを選択</a>
   <div class="w-1/4">
-    <img id="{{ $name }}_thumbnail" src="">
+    <img id="{{ $name }}_thumbnail" @if($cImage) src="{{ asset('storage/products/' . $cImage)}}" @else src="" @endif src="">
   </div>
 </div>
-<input id="{{ $name}}_hidden" type="hidden" name="{{ $name }}" value="">
+<input id="{{ $name }}_hidden" type="hidden" name="{{ $name }}" value="{{ $cId }}">

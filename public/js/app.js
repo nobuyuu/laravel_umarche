650bfe1e4b52e039b16b447c31047bd7b0fb9636
nobuyuu <yu-ya_03_20_nobu@outlook.com>
2023-08-04 21:41:18 +0900

    swiper

--- public/js/app.js
+++ public/js/app.js
@@ -5045,6 +5045,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var alpinejs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! alpinejs */ "./node_modules/alpinejs/dist/module.esm.js");
 
 
+// import '.swiper';
+
+
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_1__["default"];
 alpinejs__WEBPACK_IMPORTED_MODULE_1__["default"].start();
 

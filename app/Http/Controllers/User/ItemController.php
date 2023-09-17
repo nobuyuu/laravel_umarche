e1fd6d4867fe388d38eac273989cdfb520c7eb97
nobuyuu <yu-ya_03_20_nobu@outlook.com>
2023-09-17 17:19:34 +0900

    コンストラクタの修正

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -14,6 +14,19 @@ class ItemController extends Controller
     public function __construct()
     {
         $this->middleware('auth:users');
+
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('item'); //shopのid取得
+            if(!is_null($id)){ // null判定
+                $itemId = Product::availableItems()->where('products.id', $id)->exists();
+                if(!$itemId){ // 同じでなかったら
+                    abort(404); // 404画面表示
+                }
+            }
+            return $next($request);
+        });
+
     }
 
     public function index()
